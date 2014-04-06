cff924890435d3ac7e9fa2adba61691fa24b1a6c
Radsaggi <ashutosh.ee12@iitp.ac.in>
2014-04-06 06:22:03 +0530

    Added some useful tests

--- test/ApplicationSpec.scala
+++ test/ApplicationSpec.scala
@@ -5,26 +5,54 @@ import org.specs2.mutable.Specification
 import play.api.test._
 import play.api.test.Helpers._
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- * For more information, consult the wiki.
- */
+import controllers.BetLiMSApplication
+import controllers.FormEncapsulators._
+import controllers.Models._
 
-class ApplicationSpec extends Specification {
+class ApplicationSpec extends BetLiMSSpec {
 
   "Application" should {
 
     "send 404 on a bad request" in new WithApplication {
       route(FakeRequest(GET, "/boum")) must beNone
+      route(FakeRequest(GET, "/index")) must beNone
+      //route(FakeRequest(GET, "/search?book=")) must beNone
     }
 
-    "render the index page" in new WithApplication {
-      val home = route(FakeRequest(GET, "/")).get
+    "render the index page for GET url /" in new WithApplication {
+      val result = route(FakeRequest(GET, "/")).get
 
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
+      status(result) must equalTo(OK)
+      contentType(result) must beSome.which(_ == "text/html")
+      contentAsString(result) must contain ("Your new application is ready.")
+    }
+
+    "render the search page for GET url /search" in new WithApplication {
+      val result = route(FakeRequest(GET, "/search")).get
+
+      status(result) must equalTo(OK)
+      contentType(result) must beSome.which(_ == "text/html")
+      contentAsString(result) must contain ("Please Enter the Search query")
+      //contentAsString(result) must not contain("Search Results")
+    }
+  }
+
+  "BetLiMS Application" should {
+
+    "cater to the search GET request" in withMockDatabase { db =>
+      db.booksearch(any[BookSearch]) returns List[Book](
+        Book("12356", "Intro 2", "Ashutosh", 1),
+        Book("13356", "Intro 3", "XYZtosh", 1)
+      )
+
+      val app = mock[BetLiMSApplication]
+      app.databaseService returns db
+
+      val request = FakeRequest("GET", "/search?title=Ashu")
+      val result = app.searchPost().apply(request)
+
+      val bodyText = contentAsString(result)
+      bodyText must be equalTo "ok"
     }
   }
 }
--- test/DatabaseSpec.scala
+++ test/DatabaseSpec.scala
@@ -8,35 +8,26 @@ import controllers.Models._
 
 class DatabaseSpec extends BetLiMSSpec {
 
-  "DatabaseService" should {
-
-    "send 404 on a bad request" in withMockDatabase { _ =>
-      route(FakeRequest(GET, "/boum")) must beNone
-    }
-
-    "render the index page" in new WithApplication {
-      val home = route(FakeRequest(GET, "/")).get
-
-      status(home) must equalTo(OK)
-      contentType(home) must beSome.which(_ == "text/html")
-      contentAsString(home) must contain ("Your new application is ready.")
-    }
-  }
-
-  "SlickDatabaseServer" should {
+  "SlickDatabaseService" should {
 
     "search for a book" in withInMemoryDatabase { db =>
 
       db insertBooks Seq (
         Book("12345", "Intro 1", "Ashu", 4),
         Book("12356", "Intro 2", "Ashutosh", 1),
-        Book("13356", "Intro 3", "XYZ", 1)
+        Book("13356", "Intro 3", "XYZtosh", 1)
       )
 
-      val bs = BookSearch(None, None, Some("Ashu"))
-      db.booksearch(bs) must beEqualTo(List[Book](
+      val bs1 = BookSearch(None, None, Some("Ashu"))
+      db.booksearch(bs1) must beEqualTo(List[Book](
         Book("12345", "Intro 1", "Ashu", 4),
         Book("12356", "Intro 2", "Ashutosh", 1)
+      ))
+
+      val bs2 = BookSearch(None, None, Some("tosh"))
+      db.booksearch(bs2) must beEqualTo(List[Book](
+        Book("12356", "Intro 2", "Ashutosh", 1),
+        Book("13356", "Intro 3", "XYZtosh", 1)
       ))
     }
   }
